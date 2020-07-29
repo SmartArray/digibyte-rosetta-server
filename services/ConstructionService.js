3edba8ebf953c0b490f1afae39f80c8f5d4dd715
Yoshi <yoshi@jaeger.berlin>
2020-07-29 17:01:32 +0200

    Initial Commit

---
+++ services/ConstructionService.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2020 DigiByte Foundation NZ Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+const RosettaSDK = require('../../../..');
+
+/* Data API: Construction */
+
+/**
+* Get Transaction Construction Metadata
+* Get any information required to construct a transaction for a specific network. Metadata returned here could be a recent hash to use, an account sequence number, or even arbitrary chain state. It is up to the client to correctly populate the options object with any network-specific details to ensure the correct metadata is retrieved.  It is important to clarify that this endpoint should not pre-construct any transactions for the client (this should happen in the SDK). This endpoint is left purposely unstructured because of the wide scope of metadata that could be required.  In a future version of the spec, we plan to pass an array of Rosetta Operations to specify which metadata should be received and to create a transaction in an accompanying SDK. This will help to insulate the client from chain-specific details that are currently required here.
+*
+* constructionMetadataRequest ConstructionMetadataRequest 
+* returns ConstructionMetadataResponse
+* */
+const constructioMetadata = (params) => {
+  const { constructionMetadataRequest } = params;
+  return {};
+};
+
+/**
+* Submit a Signed Transaction
+* Submit a pre-signed transaction to the node. This call should not block on the transaction being included in a block. Rather, it should return immediately with an indication of whether or not the transaction was included in the mempool.  The transaction submission response should only return a 200 status if the submitted transaction could be included in the mempool. Otherwise, it should return an error.
+*
+* constructionSubmitRequest ConstructionSubmitRequest 
+* returns ConstructionSubmitResponse
+* */
+const constructionSubmit = (params) => {
+  const { constructionSubmitRequest } = params;
+  return {};
+};
+
+module.exports = {
+  /* /construction/metadata */
+  constructioMetadata,
+
+  /* /construction/submit */
+  constructionSubmit,
+};
