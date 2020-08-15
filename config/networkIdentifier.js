d8ae8fca8477aee87a97514ad0e3534546e42dbf
Yoshi <yoshi@jaeger.berlin>
2020-08-15 09:58:19 +0200

    Node was using the wrong network identifier (defaulting to mainnet)

--- config/networkIdentifier.js
+++ config/networkIdentifier.js
@@ -2,7 +2,7 @@ const RosettaSDK = require('rosetta-node-sdk');
 const Types = RosettaSDK.Client;
 
 const Blockchain = 'dgb';
-const Network = process.env.NETWORK || 'mainnet';
+const Network = process.env.DGB_NETWORK || 'mainnet';
 const networkIdentifier = new Types.NetworkIdentifier(Blockchain, Network);
 
 module.exports = networkIdentifier;
